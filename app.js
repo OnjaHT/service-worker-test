232a160da94c46f11c8843b457b0afb22cf43e04
onja <onja.nmoh@gmail.com>
2018-01-09 17:51:18 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+if ('serviceWorker' in navigator) {
+    var folder = '/service-worker-test/';
+    navigator.serviceWorker.register( 
+        folder + 'sw.js', 
+        { 
+            scope: folder 
+        }
+    ).then(function(reg) {
+        if (reg.installing) {
+            console.log('Service worker installing');
+        } else if (reg.waiting) {
+            console.log('Service worker installed');
+        } else if (reg.active) {
+            console.log('Service worker active');
+        }
+    }).catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+    });
+}
