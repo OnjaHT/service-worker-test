d8d0d1ff20e242a6a86683561bc3a4715e97df4f
onja <onja.nmoh@gmail.com>
2018-01-11 12:25:33 +0300

    Little fix

--- service-worker.js
+++ service-worker.js
@@ -28,30 +28,31 @@ self.addEventListener('activate', function() {
  * @since 1.0.0
  */
 self.addEventListener('fetch', function(event) {
+    console.log('Fetch [' + event.request + ']')
     event.respondWith(
         //Retourne l'objet en cache
         caches.match(event.request)
-        .catch(function() { //Si l'objet n'est pas en cache
-            console.log('FETCH > Ressource pas en cache => ', event.request );
+        // .catch(function() { //Si l'objet n'est pas en cache
+        //     console.log('FETCH > Ressource pas en cache => ', event.request );
 
-            //Recupere l'objet depuis son URL
-            return fetch(event.request)
-            .then(function(response) { //Si pas d'erreur 
-                let responseClone = response.clone();
+        //     //Recupere l'objet depuis son URL
+        //     return fetch(event.request)
+        //     .then(function(response) { //Si pas d'erreur 
+        //         let responseClone = response.clone();
 
-                caches.open(CACHE_VERSION) //On ouvre le cache
-                .then(function(cache) {
-                    cache.put(event.request, responseClone); //On l'ajoute au cache
-                })
-                .catch(function() {
-                    console.log('FETCH > Error open cache');
-                });
-            })
-            .catch(function() { //Si la resource n'est pas accessible
-                console.log('FETCH > Error 404 => ', event.request);
-                throw new Error('FETCH > Error 404 => ', event.request);
-            });
-        })
+        //         caches.open(CACHE_VERSION) //On ouvre le cache
+        //         .then(function(cache) {
+        //             cache.put(event.request, responseClone); //On l'ajoute au cache
+        //         })
+        //         .catch(function() {
+        //             console.log('FETCH > Error open cache');
+        //         });
+        //     })
+        //     .catch(function() { //Si la resource n'est pas accessible
+        //         console.log('FETCH > Error 404 => ', event.request);
+        //         throw new Error('FETCH > Error 404 => ', event.request);
+        //     });
+        // })
     );
 });
 
